1ce1c68e8c30f12e0f44af9a4a4b89d65816e739
COREProgrammers <coreprogrammers2062@googlegroups.com>
2025-08-19 17:36:58 -0500

    Increased L4 heigth for the elevator

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -218,7 +218,7 @@ public final class Constants {
         public static final double kReefStage2 = 27.775;
         public static final double kReefStage3 = 43.325;
         public static final double kReefStage4 = 67.65; // was 74.15
-        public static final double kReefStage4TipHeight = 69.65; // was 74.15
+        public static final double kReefStage4TipHeight = 71.65; // was 74.15
 
 
         public static ElevatorConfigs motorConfigs = new ElevatorConfigs();
